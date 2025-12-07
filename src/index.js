91ae77d0237aa2abf22f29aa5865f5d92dbebb62
azamjonbro <azamjondasturchi11@gmail.com>
2025-12-08 03:41:50 +0500

    added

--- src/index.js
+++ src/index.js
@@ -31,5 +31,5 @@ mongoose
   .catch((err) => console.error(err));
 
 // SERVER
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3322;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
