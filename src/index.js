31301c1dfd18160fe097ce5ba518b65895275547
azamjonbro <azamjondasturchi11@gmail.com>
2025-12-09 19:46:42 +0500

    added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors")
 const mongoose = require("mongoose");
 require("dotenv").config();
 const swaggerJsdoc = require("swagger-jsdoc");
@@ -8,6 +9,7 @@ const swaggerUi = require("swagger-ui-express");
 
 // JSON parser
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
 // STATIC FILES – rasm va boshqa fayllar
